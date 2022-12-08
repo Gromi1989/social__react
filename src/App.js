2254f3d3d801b1adc13b40f63c0dd313a1cc9f65
unknown <you@example.com>
2022-12-08 22:03:55 +0200

    сообщение

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./components/Header/Header";
+import Nav from "./components/NavBar/Nav";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const  App = (props) => {
+    return (
+        <BrowserRouter>
+        <div className='app-wrapper'>
+            <Header />
+            <Nav />
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path="/profile" element={<Profile
+                        profilePage={props.state.profilePage}
+                        addPost={props.addPost}
+                        updateNewPostText={props.updateNewPostText}
+                    />}/>
+                    <Route path="/dialogs" element={<Dialogs state={props.state.dialogsPage}/>}/>
+                    <Route path="/news" element={<News/>}/>
+                    <Route path="/music" element={<Music/>}/>
+                    <Route path="/settings" element={<Settings/>}/>
+                </Routes>
+            </div>
+        </div>
+        </BrowserRouter>
+    );
+};
 export default App;
+
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./Dialogs.module.css";
+import DialogItem from "./DialodItem/DialogItem";
+import Message from "./Message/Message";
+
+
+const Dialogs = (props) => {
+
+    let dialogsElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+    let messageElement = props.state.messages.map(message => <Message message={message.messages}/>)
+
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={classes.messages}>
+                {messageElement}
+            </div>
+        </div>
+
+    );
+};
+export default Dialogs;
